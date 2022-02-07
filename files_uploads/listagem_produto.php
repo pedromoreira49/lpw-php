d861ffb59653b7c7591bd8b58870fff78bed56b3
pedro moreira <jpedrostonemoreira@gmail.com>
2022-02-07 20:58:52 -0300

    Adicionado arquivo que realiza a listagem de produtos

---
+++ files_uploads/listagem_produto.php
@@ -0,0 +1,27 @@
+<?php
+
+include "conecta.php";
+
+$sql = "select * from produto order by codproduto";
+
+
+$resultado = mysqli_query($conexao,$sql);
+
+while($linha=mysqli_fetch_assoc($resultado)) {
+
+?>
+<section>
+<form action="edicao_produto.php" method="post">
+<p>ID: <?php echo $linha['codproduto']; ?></p>
+<input type ="hidden" name = "codproduto" value="<?php echo $linha['codproduto']?>">
+<p>Nome:  <input type="text" name="nomeprod" value="<?php echo $linha['nomeprod']?>"></p>
+<p>Descrição:<input type ="text" name = "descprod" value="<?php echo $linha['descprod']?>"></p> 
+<button type="submit" name="acao" value="editar"> Editar </button>
+<button type="submit" name="acao" value="excluir" onclick = "return confirma_excluir()"> Deletar </button> 
+</form>                                                         
+</section>
+<?php
+}
+?>
+<a href='index.php'>Voltar </a>
+<a href='sair.php'>Sair</a>
