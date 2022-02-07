e1e613a507a0e6b4b80f0f815f1d8634f3f706cc
pedro moreira <jpedrostonemoreira@gmail.com>
2022-02-07 20:56:28 -0300

    Adicionado arquivo que realiza a validação de login

---
+++ files_uploads/valida_login.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+$email=$_POST["email"];
+$senha=$_POST["senha"];
+if (!(empty($email) OR empty($senha))) 
+{
+	include "conecta.php";
+	$sql="SELECT * from pessoa WHERE email='$email' and senha='$senha'";
+	$resultado = mysqli_query($conexao,$sql);
+
+	if (mysqli_num_rows($resultado)== 1)
+	{
+	$_SESSION["logado"]=true; 
+	$_SESSION["email"]=$email;
+	header("Location:form_pessoa_produto.php");
+	}
+	else
+	{
+		$_SESSION["msg"]="Usuário ou senha inválidos"; 
+		header("Location:index.php");
+	}
+}
+else 
+{
+	$_SESSION["msg"]="Preencha campos email e senha";
+	header("Location:index.php");
+}
