5b9963ee2db332a063848998ae0f587bf5d31934
pedro moreira <jpedrostonemoreira@gmail.com>
2022-02-07 20:53:49 -0300

    adicionado arquivo que realiza a conexão com banco de dados

---
+++ files_uploads/conecta.php
@@ -0,0 +1,9 @@
+<?php
+$conexao = new mysqli("localhost", "root", "", "aula"); // conexao pelo método orientado a objetos - através do instanciamento de um objeto 
+//notação para acessar um método do objeto no PHP "$objeto->metodo()"
+$conexao->set_charset("utf8");
+
+if (!$conexao) {
+    die('Não foi possível conectar: ' . mysql_error());
+}
+?>
