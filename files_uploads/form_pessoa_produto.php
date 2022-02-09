3e277ef651dbe37d6416d7af9f11a213588072dc
pedro moreira <jpedrostonemoreira@gmail.com>
2022-02-09 17:53:48 -0300

    Modificado arquivo para um dashboard da aplicação

--- files_uploads/form_pessoa_produto.php
+++ files_uploads/form_pessoa_produto.php
@@ -9,27 +9,77 @@ if((!isset($_SESSION['email'])) && (!isset($_SESSION['logado'])))
 
 ?>
 
-
-<h1>Cadastro Pessoa</h1>
-    <form action="cadastro_usuario.php" method="POST" enctype="multipart/form-data">
-    Nome : <input type="text" name="nome">
-    Email : <input type="text" name="email">
-    CPF: <input type="text" name="cpf">
-    Senha: <input type="password" name="senha">
-    <p>Foto: <input type="file" name="arquivo"></p>
-    <input type="reset" name="botao" value="Limpar">
-    <input type="submit" name="botao" value="Enviar">
-</form>
-
-<h1>Cadastro Produto</h1>
-    <form action="cadastro_produto.php" method="POST">
-    Nome : <input type="text" name="nomeprod">
-    Descrição : <input type="text" name="descprod">
-    <input type="reset" name="botao" value="Limpar">
-    <input type="submit" name="botao" value="Enviar">
-</form>
-
-<a href='listagem_usuario.php'>Edição de Usuários</a>
-<a href='listagem_produto.php'>Edição de Produtos</a>
-<a href='avaliacoes_produtos.php'>Avaliações de Produtos</a>
-<a href='sair.php'>Sair</a>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>lpw-tsi dashboard</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <section class="nav-container">
+        <?php
+            include('nav-bar.php');
+        ?>
+    </section>
+    <section class="main-container">
+        <div class="wrapper-2">
+            <div class="inner-warpper">
+                <div class="text-center">
+                    <h2>Cadastro Pessoa:</h2>
+                </div>
+                <form action="cadastro_usuario.php" method="post" id="formvalidate">
+                    <div class="input-group">
+                        <label class="palceholder" for="nome"></label>
+                        <input class="form-control" name="nome" id="userName" type="text" placeholder="Nome" required/>
+                        <span class="lighting"></span>
+                    </div>
+                    <div class="input-group">
+                        <label class="palceholder" for="email"></label>
+                        <input class="form-control" name="email" id="userName" type="text" placeholder="Email" required/>
+                        <span class="lighting"></span>
+                    </div>
+                    <div class="input-group">
+                        <label class="palceholder" for="cpf"></label>
+                        <input class="form-control" name="cpf" id="userName" type="text" placeholder="Cpf" required/>
+                        <span class="lighting"></span>
+                    </div>
+                    <div class="input-group">
+                        <label class="palceholder" for="senha"></label>
+                        <input class="form-control" name="senha" id="userPassword" type="password" placeholder="Senha" required/>
+                        <span class="lighting"></span>
+                    </div>
+                    <div class="input-group">
+                        <label class="palceholder" for="arquivo"></label>
+                        <input class="form-control" name="arquivo" type="file"/>
+                        <span class="lighting"></span>
+                    </div>
+                    <button type="submit" name="acao"id="login">Cadastrar</button>
+                    <button type="reset" name="acao"id="login">Reset</button>
+                </form>
+            </div>
+        </div>
+        <div class="wrapper-2">
+            <div class="inner-warpper">
+                <div class="text-center">
+                    <h2>Cadastro produto:</h2>
+                </div>
+                <form action="cadastro_produto.php" method="post" id="formvalidate">
+                    <div class="input-group">
+                        <label class="palceholder" for="nomeprod"></label>
+                        <input class="form-control" name="nomeprod" id="userName" type="text" placeholder="Nome do produto" required/>
+                        <span class="lighting"></span>
+                    </div>
+                    <div class="input-group">
+                        <label class="palceholder" for="descprod"></label>
+                        <input class="form-control" name="descprod" id="userName" type="text" placeholder="Descrição do produto" required/>
+                        <span class="lighting"></span>
+                    </div>
+                    <button type="submit" name="acao"id="login">Cadastrar</button>
+                    <button type="reset" name="acao"id="login">Reset</button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
