fec49aa2a812b54f566be9addb4fe30ec6c0a817
pedro moreira <jpedrostonemoreira@gmail.com>
2022-02-09 12:03:26 -0300

    Modificado arquivo de listagem de usuarios

--- files_uploads/listagem_usuario.php
+++ files_uploads/listagem_usuario.php
@@ -15,12 +15,12 @@ while($linha=mysqli_fetch_assoc($resultado)) {
 <p>ID: <?php echo $linha['codpessoa']; ?></p>
 <input type ="hidden" name = "codpessoa" value="<?php echo $linha['codpessoa']?>">
 <p>Nome:  <input type="text" name="nome" value="<?php echo $linha['nome']?>"></p>
-<p>Email:<input type ="text" name = "email" value="<?php echo $linha['email']?>"></p> 
+<p>Email:<input type ="text" name = "email" value="<?php echo $linha['email']?>"></p>
 <p>CPF: <input type ="text" name = "cpf" value="<?php echo $linha['cpf']?>" ?></p>
 <p>Senha: <input type ="password" name = "senha" value="<?php echo $linha['senha']?>" ?></p>
 <button type="submit" name="acao" value="editar"> Editar </button>
-<button type="submit" name="acao" value="excluir" onclick = "return confirma_excluir()"> Deletar </button> 
-</form>                                                         
+<button type="submit" name="acao" value="excluir" onclick = "return confirma_excluir()"> Deletar </button>
+</form>
 </section>
 <?php
 }
