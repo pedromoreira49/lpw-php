a4910e4a785e3d77ad818aa1fc98635bce9e1f87
pedro moreira <jpedrostonemoreira@gmail.com>
2022-02-09 18:22:22 -0300

    Modificado looping de apresentação dos dados dos produtos

--- files_uploads/listagem_produto.php
+++ files_uploads/listagem_produto.php
@@ -1,27 +1,15 @@
 <?php
 
 include "conecta.php";
+include 'nav-bar.php';
 
 $sql = "select * from produto order by codproduto";
 
+echo "<a href='form_pessoa_produto.php'>Voltar </a>";
 
 $resultado = mysqli_query($conexao,$sql);
 
 while($linha=mysqli_fetch_assoc($resultado)) {
-
-?>
-<section>
-<form action="edicao_produto.php" method="post">
-<p>ID: <?php echo $linha['codproduto']; ?></p>
-<input type ="hidden" name = "codproduto" value="<?php echo $linha['codproduto']?>">
-<p>Nome:  <input type="text" name="nomeprod" value="<?php echo $linha['nomeprod']?>"></p>
-<p>Descrição:<input type ="text" name = "descprod" value="<?php echo $linha['descprod']?>"></p> 
-<button type="submit" name="acao" value="editar"> Editar </button>
-<button type="submit" name="acao" value="excluir" onclick = "return confirma_excluir()"> Deletar </button> 
-</form>
-</section>
-<?php
+	include 'dados_produto.php';
 }
 ?>
-<a href='form_pessoa_produto.php'>Voltar </a>
-<a href='sair.php'>Sair</a>
