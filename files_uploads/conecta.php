803fc2233ac2f6ed8e55e7abddd8f016c4873dee
pedro moreira <jpedrostonemoreira@gmail.com>
2022-02-09 11:59:38 -0300

    modificado arquivo de conexão com banco de dados

--- files_uploads/conecta.php
+++ files_uploads/conecta.php
@@ -1,5 +1,5 @@
 <?php
-$conexao = new mysqli("localhost", "root", "", "aula"); // conexao pelo método orientado a objetos - através do instanciamento de um objeto 
+$conexao = new mysqli("localhost", "root", "jpedrostonemoreira", "aula"); // conexao pelo método orientado a objetos - através do instanciamento de um objeto 
 //notação para acessar um método do objeto no PHP "$objeto->metodo()"
 $conexao->set_charset("utf8");
 
