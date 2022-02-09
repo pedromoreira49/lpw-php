497809cadeeb347ac0fca2b47a373ef60a76b115
pedro moreira <jpedrostonemoreira@gmail.com>
2022-02-09 17:57:19 -0300

    Adicionado arquivo de navegação do dashboard

---
+++ files_uploads/nav-bar.php
@@ -0,0 +1,10 @@
+  <link rel="stylesheet" href="./nav-bar.css">
+  <header class="header">
+    <h1 class="logo"><a href="form_pessoa_produto.php">LPW-TSI</a></h1>
+      <ul class="main-nav">
+          <li><a href="listagem_usuario.php">Ed. User</a></li>
+          <li><a href="listagem_produto.php">Ed. Product</a></li>
+          <li><a href="#">Rate Product</a></li>
+          <li><a href="sair.php">Logout</a></li>
+      </ul>
+  </header> 
