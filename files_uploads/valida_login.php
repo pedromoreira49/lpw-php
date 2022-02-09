08e7285da93ab74927398c28f556654f79b4d278
pedro moreira <jpedrostonemoreira@gmail.com>
2022-02-09 12:03:46 -0300

    Modificado arquivo de validação de login

--- files_uploads/valida_login.php
+++ files_uploads/valida_login.php
@@ -1,28 +1,29 @@
 <?php
 session_start();
 
-$email=$_POST["email"];
-$senha=$_POST["senha"];
-if (!(empty($email) OR empty($senha))) 
+$email=$_POST['email'];
+$senha=$_POST['senha'];
+if (!(empty($email) OR empty($senha)))
 {
 	include "conecta.php";
-	$sql="SELECT * from pessoa WHERE email='$email' and senha='$senha'";
+	$sql="SELECT * from pessoa WHERE email='$email' and senha='$senha';";
 	$resultado = mysqli_query($conexao,$sql);
-
 	if (mysqli_num_rows($resultado)== 1)
 	{
-	$_SESSION["logado"]=true; 
-	$_SESSION["email"]=$email;
+	$_SESSION['logado']=true;
+	$_SESSION['email']=$email;
 	header("Location:form_pessoa_produto.php");
 	}
 	else
 	{
-		$_SESSION["msg"]="Usuário ou senha inválidos"; 
+		$_SESSION["msg"]="Usuário ou senha inválidos";
 		header("Location:index.php");
 	}
 }
-else 
+else
 {
 	$_SESSION["msg"]="Preencha campos email e senha";
 	header("Location:index.php");
 }
+
+?>
