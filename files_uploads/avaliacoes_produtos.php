b46007f5a72cb0e5f7682efff670e9e7521b23d2
pedro moreira <jpedrostonemoreira@gmail.com>
2022-02-09 17:52:36 -0300

    Modificado arquivo de avaliações

--- files_uploads/avaliacoes_produtos.php
+++ files_uploads/avaliacoes_produtos.php
@@ -1,9 +1,27 @@
 <?php
 	include 'conecta.php';
-	echo 'hello world';
+
+	$sql = "select * from produto order by codproduto";
+
+	$resultado = mysqli_query($conexao, $sql);
+
+	while($linha = mysqli_fetch_assoc($resultado)){	
 
 ?>
+<section>
+<form action='cadastro_avaliacao.php' method=post>
+<p>ID: <?php echo $linha['codproduto']; ?></p>	
+<p>Nome: <?php echo $linha['nomeprod']; ?></p>
+<p>Descrição <?php echo $linha['descprod']; ?></p>
+<p>Observação: <input type="text" name="obersavacao"></p>
+<p>Nota: <input type="text" name="nota"></p>
+<button type="submit" name="acao" value="Enviar">Enviar</button>
+</form>	
+</section>
 
+<?php
+}
+?>
 <br />
 <a href='form_pessoa_produto.php'>Voltar</a>
 <br />
