c6655059cd6314a009405d0ef0d18b21aae7f847
pedro moreira <jpedrostonemoreira@gmail.com>
2022-02-10 14:25:38 -0300

    Modificado arquivo de edição de usuários

--- files_uploads/dados_usuario.php
+++ files_uploads/dados_usuario.php
@@ -1,12 +1,40 @@
-<section>
-<form action="edicao_usuario.php" method="post">
-<p>ID: <?php echo $linha['codpessoa']; ?></p>
-<input type ="hidden" name = "codpessoa" value="<?php echo $linha['codpessoa']?>">
-<p>Nome:  <input type="text" name="nome" value="<?php echo $linha['nome']?>"></p>
-<p>Email:<input type ="text" name = "email" value="<?php echo $linha['email']?>"></p>
-<p>CPF: <input type ="text" name = "cpf" value="<?php echo $linha['cpf']?>" ?></p>
-<p>Senha: <input type ="password" name = "senha" value="<?php echo $linha['senha']?>" ?></p>
-<button type="submit" name="acao" value="editar"> Editar </button>
-<button type="submit" name="acao" value="excluir" onclick = "return confirma_excluir()"> Deletar </button>
-</form>
-</section>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>lpw-tsi dashboard</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<div class="wrapper-2">
+	<section>
+		<form action="edicao_usuario.php" method="post" id="formvalidate">
+			<input type ="hidden" name = "codpessoa" value="<?php echo $linha['codpessoa']?>">
+			<div class="input-group">
+		        <label class="palceholder" for="nome"></label>
+		        <input class="form-control" name="nome" id="userName" type="text" value="<?php echo $linha['nome'];?>"/>
+		        <span class="lighting"></span>
+		    </div>
+			<div class="input-group">
+		        <label class="palceholder" for="email"></label>
+		        <input class="form-control" name="email" id="userName" type="text" value="<?php echo $linha['email'];?>">
+		        <span class="lighting"></span>
+		     </div>
+		     <div class="input-group">
+		        <label class="palceholder" for="cpf"></label>
+		        <input class="form-control" name="cpf" id="userName" type="text" value="<?php echo $linha['cpf'];?>">
+		        <span class="lighting"></span>
+		     </div>
+		     <div class="input-group">
+		        <label class="palceholder" for="senha"></label>
+		        <input class="form-control" name="senha" id="userName" type="password" value="<?php echo $linha['senha'];?>">
+		        <span class="lighting"></span>
+		     </div>
+			<button id="login" type="submit" name="acao" value="editar"> Editar </button>
+			<button id="login" type="submit" name="acao" value="excluir" onclick = "return confirma_excluir()"> Deletar </button>
+		</form>
+	</section>
+</div>
+</body>
+</html>
